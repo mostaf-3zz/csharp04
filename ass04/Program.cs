3ebde2872a660fa7724279f6711d0119a9810c7a
mostafa-3zz <zzzmostafa6@gmail.com>
2025-02-06 14:49:29 +0200

    Q5

--- ass04/Program.cs
+++ ass04/Program.cs
@@ -4,18 +4,15 @@
     {
         static void Main(string[] args)
         {
-            #region Q4
-            int result = 1;
-            Console.WriteLine("please enter the base ");
-            int.TryParse(Console.ReadLine(), out int Base);
-            Console.WriteLine("please enter the foundation");
-            int.TryParse(Console.ReadLine(), out int Found);
-            for (int i = 1; i <= Found; i++)
-            {
-                result *= Base;
+            #region Q5
 
+            Console.WriteLine("please enter string");
+            string tex = Console.ReadLine();
+            for (int i = 0; i < tex.Length; i++)
+            {
+                Console.Write(tex[tex.Length - 1 - i]);
             }
-            Console.WriteLine(result);
+
             #endregion
         }
     }
