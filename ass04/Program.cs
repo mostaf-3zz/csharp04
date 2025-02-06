c3737770bc943a5c1007a875be4da37dca8101c8
mostafa-3zz <zzzmostafa6@gmail.com>
2025-02-06 14:55:28 +0200

    Q10

--- ass04/Program.cs
+++ ass04/Program.cs
@@ -4,20 +4,13 @@
     {
         static void Main(string[] args)
         {
-            #region Q9
-
-            Console.WriteLine("please enter a string ");
-            string str = Console.ReadLine();
-            int start, end;
-            Console.WriteLine("please enter start");
-            start = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("please enter end");
-            end = int.Parse(Console.ReadLine());
-            for (int i = start; i <= end; i++)
-            {
-                Console.Write(str[i]);
-            }
+            #region Q10
+            Console.WriteLine("please enter first string");
+            string st1 = Console.ReadLine();
+            Console.WriteLine("please enter  second string ");
+            string st2 = Console.ReadLine();
+            string FullSt = st1 + st2;
+            Console.WriteLine(FullSt);
             #endregion
         }
     }
