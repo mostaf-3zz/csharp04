40378b1be5d94ee6638ab6c108b4aadc5e9c0bca
mostafa-3zz <zzzmostafa6@gmail.com>
2025-02-06 14:51:12 +0200

    Q6

--- ass04/Program.cs
+++ ass04/Program.cs
@@ -4,15 +4,16 @@
     {
         static void Main(string[] args)
         {
-            #region Q5
-
-            Console.WriteLine("please enter string");
-            string tex = Console.ReadLine();
-            for (int i = 0; i < tex.Length; i++)
+            #region Q6
+            Console.WriteLine("please enter a start");
+            int Start = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter a end ");
+            int End = int.Parse(Console.ReadLine());
+            for (int i = Start; i <= End; ++i)
             {
-                Console.Write(tex[tex.Length - 1 - i]);
-            }
 
+                Console.WriteLine($"{i}");
+            }
             #endregion
         }
     }
