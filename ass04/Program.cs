5f3d1902ba60fd80a352c7eabeac55ff81412215
mostafa-3zz <zzzmostafa6@gmail.com>
2025-02-06 14:56:13 +0200

    Q12

--- ass04/Program.cs
+++ ass04/Program.cs
@@ -4,13 +4,19 @@
     {
         static void Main(string[] args)
         {
-            #region Q10
-            Console.WriteLine("please enter first string");
-            string st1 = Console.ReadLine();
-            Console.WriteLine("please enter  second string ");
-            string st2 = Console.ReadLine();
-            string FullSt = st1 + st2;
-            Console.WriteLine(FullSt);
+            #region Q12
+            Console.WriteLine("Please enter the size of array");
+            int size = int.Parse(Console.ReadLine()), sum = 0;
+            int[] Numbers = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"please ener array element no {i}");
+                Numbers[i] = int.Parse(Console.ReadLine());
+                sum += Numbers[i];
+
+            }
+            Console.WriteLine($"Sum of elements of array is {sum}");
             #endregion
         }
     }
